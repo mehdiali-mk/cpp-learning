504440bcd52d65c393421a206f4efcec8c693a26
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-01-16 13:08:02 +0530

    Print Address Program is added in DAY - 1

---
+++ DAY-1/02_Print_Address.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Address:" << endl
+         << "14 - Silver Park" << endl
+         << "Jawaharnagar" << endl
+         << "Delhi - 380033" << endl;
+
+    return 0;
+}
