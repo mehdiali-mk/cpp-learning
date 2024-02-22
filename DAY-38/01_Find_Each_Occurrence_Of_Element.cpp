9f96e9a23dec70ab53b22d149d127babb3db36fc
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-02-22 20:21:09 +0530

    DAY 38 - Folder is added.

---
+++ DAY-38/01_Find_Each_Occurrence_Of_Element.cpp
@@ -0,0 +1,47 @@
+/*
+ Aim = C Program to Find Occurrences of Each Element of an Array
+ Author = Mehdiali (MK).
+ Date = 22 / February / 2024 - 05:57 PM.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+
+    cout << "Enter the size of an array = ";
+    cin >> size;
+
+    int arr[size], visited[size], count;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "arr[" << i << "] = ";
+        cin >> arr[i];
+        visited[i] = 0;
+    }
+
+    cout << "\n";
+    for (int i = 0; i < size; i++)
+    {
+        if (visited[i] == 1)
+        {
+            continue;
+        }
+        count = 1;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        cout << arr[i] << " found " << count << " times in an array." << endl;
+    }
+
+    return 0;
+}
